5e2dc50f787995cf882ffe05bf4b32c442394b13
De'shon Mcneil <miad2401@gmail.com>
2023-01-20 18:46:48 -0500

    Initial commit

---
+++ ITCS 3112 ICON Character Creator/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITCS_3112_ICON_Character_Creator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ ITCS 3112 ICON Character Creator/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITCS_3112_ICON_Character_Creator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControlSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabControl control = (TabControl)sender;
+            if (control != null)
+            {
+                if (control.SelectedItem != null)
+                {
+                    TabItem selected = control.SelectedItem as TabItem;
+                    this.Title = selected.Header.ToString();
+                    selected.Background = new BrushConverter().ConvertFrom("#FF9A7767") as Brush;
+                }
+
+            }
+        }
+    }
+}
